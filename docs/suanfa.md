c8d36d430d8010b532f52ed03cf645a7d4c1ef3a
hohao234 <hohao234@163.com>
2026-07-19 21:46:48 +0800

    Initial commit - 配置更新

--- docs/suanfa.md
+++
@@ -1,5 +0,0 @@
-# 关姐算法搭建说明
-
-php7.3.3 + mysql5.7.4
-
-php程序包，推荐通过1panel面板php7环境进行搭建。需要安装sg扩展如 `sg` `sg11`,`sg16` `pdo_mysql`
--- public/monkey/odoo-field-getter.js
+++
@@ -1,6 +0,0 @@
-function getFieldInfo(name) {
-    const $label = $(`label:contains("${name}")`);
-    return $label.find('sup').attr('data-tooltip-info')
-}
-
-getFieldInfo('类别代码');
